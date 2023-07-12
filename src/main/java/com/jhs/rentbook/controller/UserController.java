1bc8757075fd2177d595fbeac85cc8b841c00600
JHS <wnghdtjr129@naver.com>
2023-07-12 15:39:39 +0900

    feat: UserController 기능 구현

--- src/main/java/com/jhs/rentbook/controller/UserController.java
+++ src/main/java/com/jhs/rentbook/controller/UserController.java
@@ -3,11 +3,13 @@ package com.jhs.rentbook.controller;
 import com.jhs.rentbook.controller.dto.IdResponse;
 import com.jhs.rentbook.controller.dto.rental.RentalBookInfo;
 import com.jhs.rentbook.controller.dto.user.*;
+import com.jhs.rentbook.domain.rental.UserBookRental;
+import com.jhs.rentbook.domain.user.User;
+import com.jhs.rentbook.domain.user.vo.UserVo;
 import com.jhs.rentbook.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -18,21 +20,32 @@ public class UserController {
 
     @GetMapping("/users")
     public List<UserInfo> getAllUserList() {
-        return new ArrayList<>();
+        return userService.getUserList().stream()
+                .map(User::values)
+                .map(value -> new UserInfo(value.userId(), value.username(), value.email(), value.role()))
+                .toList();
     }
 
     @PostMapping("/user/login")
     public IdResponse login(@RequestBody LoginRequest request) {
-        return new IdResponse(0L);
+        UserVo value = userService.login(request.email(), request.password()).values();
+
+        return new IdResponse(value.userId());
     }
 
     @PostMapping("/user")
     public SignUpResponse signUpUser(@RequestBody SignUpRequest request) {
-        return new SignUpResponse(0L, "");
+        User user = User.of(request.email(), request.password(), request.name());
+        UserVo value = userService.saveUser(user).values();
+
+        return new SignUpResponse(value.userId(), value.email());
     }
 
     @GetMapping("/user/{userId}/rental")
     public List<RentalBookInfo> getOwnRentalBooks(@PathVariable Long userId) {
-        return new ArrayList<>();
+        return userService.getRentalBookList(userId).stream()
+                .map(UserBookRental::values)
+                .map(value -> new RentalBookInfo(value.bookValue().id(), value.bookValue().name(), value.bookValue().type()))
+                .toList();
     }
 }
