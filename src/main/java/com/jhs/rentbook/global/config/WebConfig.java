6c48be12e63706361961202cb32dda427b79efe0
JHS <wnghdtjr129@naver.com>
2023-07-13 22:11:52 +0900

    feat: 필터 등록

--- src/main/java/com/jhs/rentbook/global/config/WebConfig.java
+++ src/main/java/com/jhs/rentbook/global/config/WebConfig.java
@@ -1,7 +1,11 @@
 package com.jhs.rentbook.global.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jhs.rentbook.global.filter.AuthenticationFilter;
+import com.jhs.rentbook.global.filter.AuthenticationStorage;
 import com.jhs.rentbook.global.filter.AuthorizationFilter;
+import com.jhs.rentbook.global.filter.ExceptionHandlerFilter;
+import com.jhs.rentbook.repository.UserRepository;
 import jakarta.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -11,10 +15,10 @@ import org.springframework.context.annotation.Configuration;
 public class WebConfig {
 
     @Bean
-    public FilterRegistrationBean<Filter> authenticationFilter() {
+    public FilterRegistrationBean<Filter> exceptionHandlerFilter(ObjectMapper mapper) {
         FilterRegistrationBean<Filter> registration = new FilterRegistrationBean<>();
 
-        registration.setFilter(new AuthenticationFilter());
+        registration.setFilter(new ExceptionHandlerFilter(mapper));
         registration.setOrder(1);
         registration.addUrlPatterns("/*");
 
@@ -22,11 +26,22 @@ public class WebConfig {
     }
 
     @Bean
-    public FilterRegistrationBean<Filter> authorizationFilter() {
+    public FilterRegistrationBean<Filter> authenticationFilter(AuthenticationStorage storage, UserRepository repository) {
         FilterRegistrationBean<Filter> registration = new FilterRegistrationBean<>();
 
-        registration.setFilter(new AuthorizationFilter());
+        registration.setFilter(new AuthenticationFilter(storage, repository));
         registration.setOrder(2);
+        registration.addUrlPatterns("/*");
+
+        return registration;
+    }
+
+    @Bean
+    public FilterRegistrationBean<Filter> authorizationFilter(AuthenticationStorage storage) {
+        FilterRegistrationBean<Filter> registration = new FilterRegistrationBean<>();
+
+        registration.setFilter(new AuthorizationFilter(storage));
+        registration.setOrder(3);
         registration.addUrlPatterns("/*");
 
         return registration;
