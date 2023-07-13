188f9a270f54c2fdde1fbc716cb265f0a94478eb
JHS <wnghdtjr129@naver.com>
2023-07-13 22:11:44 +0900

    feat: 필터에서 발생한 예외 후처리 필터 구현

---
+++ src/main/java/com/jhs/rentbook/global/filter/ExceptionHandlerFilter.java
@@ -0,0 +1,37 @@
+package com.jhs.rentbook.global.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jhs.rentbook.global.exception.ExceptionResponse;
+import com.jhs.rentbook.global.exception.custom.BusinessException;
+import com.jhs.rentbook.global.exception.custom.filter.AuthorizationException;
+import jakarta.servlet.*;
+import lombok.RequiredArgsConstructor;
+
+import java.io.IOException;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RequiredArgsConstructor
+public class ExceptionHandlerFilter implements Filter {
+
+    private final ObjectMapper mapper;
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        String responseMessage = "";
+        try {
+            chain.doFilter(request, response);
+        } catch (BusinessException exception) {
+            ExceptionResponse value = new ExceptionResponse(BAD_REQUEST.value(), exception.getMessage());
+            responseMessage = mapper.writeValueAsString(value);
+        } catch (AuthorizationException exception) {
+            ExceptionResponse exceptionResponse = new ExceptionResponse(FORBIDDEN.value(), exception.getMessage());
+            responseMessage = mapper.writeValueAsString(exceptionResponse);
+        }
+
+        response.setContentType(APPLICATION_JSON_VALUE);
+        response.getOutputStream().println(responseMessage);
+    }
+}
