650694660dc800e69aff344d70f9c6f2e25e224f
JHS <wnghdtjr129@naver.com>
2023-07-14 20:58:20 +0900

    refactor: requestMatcher 등록기 분리

--- src/main/java/com/jhs/rentbook/global/filter/AuthorizationFilter.java
+++ src/main/java/com/jhs/rentbook/global/filter/AuthorizationFilter.java
@@ -2,6 +2,7 @@ package com.jhs.rentbook.global.filter;
 
 import com.jhs.rentbook.global.exception.custom.filter.AuthorizationException;
 import com.jhs.rentbook.global.filter.AuthenticationStorage.StorageField;
+import com.jhs.rentbook.global.filter.matcher.RequestMatcherRegistry;
 import com.jhs.rentbook.global.filter.matcher.UserIdDynamicRequestMatcher;
 import com.jhs.rentbook.global.filter.matcher.RequestMatcher;
 import jakarta.servlet.*;
@@ -21,51 +22,19 @@ import static org.springframework.http.HttpMethod.*;
 public class AuthorizationFilter implements Filter {
 
     private final AuthenticationStorage storage;
-    private static final String NO_ROLE = "NO_ROLE";
-
-    private final Map<String, List<RequestMatcher>> accessUris = Map.of(
-            "USER", List.of(new RequestMatcher(GET, "/books"), new RequestMatcher(DELETE, "/rental/*"), new RequestMatcher(POST, "/rental/book"),
-                    new UserIdDynamicRequestMatcher(GET, "/user/*/rental")),
-            "MANAGER", List.of(new RequestMatcher(POST, "/book"), new RequestMatcher(GET, "/rental/books"), new RequestMatcher(GET, "/users")
-                    , new RequestMatcher(GET, "/books")),
-            NO_ROLE, List.of(new RequestMatcher(POST, "/user/login"), new RequestMatcher(POST, "/user"))
-    );
+    private final RequestMatcherRegistry registry;
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         StorageField field = storage.get();
         HttpServletRequest request = (HttpServletRequest) req;
 
-        if (canNotAccess(field, request)) {
+        if (registry.canNotAccess(field, request)) {
             throw new AuthorizationException("Has No Access Authorize");
         }
 
         chain.doFilter(req, response);
     }
 
-    private boolean canNotAccess(StorageField field, HttpServletRequest request) {
-        String role = field == null ? NO_ROLE : field.role();
-        List<RequestMatcher> requestMatchers = accessRequestMatchers(role);
 
-        boolean result = false;
-        for (RequestMatcher requestMatcher : requestMatchers) {
-            result = requestMatcher.isMatch(request, field);
-
-            if (result) {
-                break;
-            }
-        }
-
-        return !result;
-    }
-
-    private List<RequestMatcher> accessRequestMatchers(String role) {
-        List<RequestMatcher> totalAccessUris = new ArrayList<>();
-        totalAccessUris.addAll(accessUris.get(role));
-        if (!role.equals(NO_ROLE)) {
-            totalAccessUris.addAll(accessUris.get(NO_ROLE));
-        }
-
-        return totalAccessUris;
-    }
 }
