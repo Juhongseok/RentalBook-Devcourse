2ad61020d062abb2f823cee79a6b17ae405d341a
JHS <wnghdtjr129@naver.com>
2023-07-14 16:24:33 +0900

    test: book controller 통합 테스트 코드 작성

---
+++ src/test/java/com/jhs/rentbook/integration/BookIntegrationTest.java
@@ -0,0 +1,75 @@
+package com.jhs.rentbook.integration;
+
+import com.jhs.rentbook.controller.dto.IdResponse;
+import com.jhs.rentbook.controller.dto.book.SaveBookRequest;
+import com.jhs.rentbook.controller.dto.user.SignUpRequest;
+import com.jhs.rentbook.controller.dto.user.SignUpResponse;
+import com.jhs.rentbook.domain.book.BookType;
+import com.jhs.rentbook.global.exception.ExceptionResponse;
+import com.jhs.rentbook.integration.base.AbstractIntegrationTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.http.HttpMethod.POST;
+
+class BookIntegrationTest extends AbstractIntegrationTest {
+
+    @Test
+    @DisplayName("create Books 10")
+    void createBulkBooks() {
+        List<IdResponse> idResponses = bulkCreateBook();
+
+        assertThat(idResponses).hasSize(10);
+    }
+
+    @Test
+    @DisplayName("fail create books has no authorization")
+    void failCreateBook() {
+        SignUpResponse userResponse = createDummyUser();
+        HttpEntity<Object> entity = convertHttpEntity(null, String.valueOf(userResponse.userId()));
+
+        ResponseEntity<ExceptionResponse> responseEntity = restTemplate.exchange(baseUrl + "/book", HttpMethod.GET, entity, ExceptionResponse.class);
+        ExceptionResponse response = responseEntity.getBody();
+
+        assertThat(response.statusCode()).isEqualTo(403);
+    }
+
+    private SignUpResponse createDummyUser() {
+        SignUpRequest request = new SignUpRequest("user@gmail.com", "Password1!", "User1");
+
+        return restTemplate
+                .postForEntity(baseUrl + "/user", request, SignUpResponse.class)
+                .getBody();
+    }
+
+    private List<IdResponse> bulkCreateBook() {
+        List<IdResponse> responses = new ArrayList<>();
+        String requestUrl = baseUrl + "/book";
+
+        for (int i = 0; i < 10; i++) {
+            IdResponse response = createBook(requestUrl, i);
+            responses.add(response);
+        }
+
+        return responses;
+    }
+
+    private IdResponse createBook(String requestUrl, int indexNumber) {
+        String bookName = "book" + indexNumber;
+        String bookType = indexNumber % 2 == 0 ? BookType.DEVELOP.name() : BookType.LANGUAGE.name();
+        SaveBookRequest request = new SaveBookRequest(bookName, bookType);
+        HttpEntity<SaveBookRequest> entity = convertHttpEntity(request, "1");
+
+        return restTemplate
+                .exchange(requestUrl, POST, entity, IdResponse.class)
+                .getBody();
+    }
+
+}
--- src/test/java/com/jhs/rentbook/integration/base/AbstractIntegrationTest.java
+++ src/test/java/com/jhs/rentbook/integration/base/AbstractIntegrationTest.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 
 import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
@@ -16,6 +18,8 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 @TestInstance(PER_CLASS)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public abstract class AbstractIntegrationTest {
+
+    private static final String HEADER_NAME = "LOGIN-ID";
 
     @LocalServerPort
     private int port;
@@ -28,6 +32,13 @@ public abstract class AbstractIntegrationTest {
     @BeforeEach
     void setUp() {
         baseUrl = "http://localhost:".concat(port + "");
+    }
+
+    protected  <T> HttpEntity<T> convertHttpEntity(T request, String headerValue) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(HEADER_NAME, headerValue);
+
+        return new HttpEntity<>(request, headers);
     }
 
 }
