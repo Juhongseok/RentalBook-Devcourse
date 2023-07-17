b2673bf4a81d47b814c2872f56fa4cec8a4553cc
JHS <wnghdtjr129@naver.com>
2023-07-17 13:33:34 +0900

    fix: 권한 관리 수정

--- src/main/java/com/jhs/rentbook/global/filter/matcher/request/RequestMatcherRegistry.java
+++ src/main/java/com/jhs/rentbook/global/filter/matcher/request/RequestMatcherRegistry.java
@@ -16,16 +16,15 @@ import static org.springframework.http.HttpMethod.*;
 public class RequestMatcherRegistry {
 
     private final Map<String, List<RequestMatcher>> accessUris = Map.of(
-            "USER", List.of(new RequestMatcher(GET, "/books"), new RequestMatcher(DELETE, "/rental/*"), new RequestMatcher(POST, "/rental/book"),
-                    new RequestMatcher(POST, "/book/*/rental"), new RequestMatcher(GET, "/book/rental/user/*")),
-            "MANAGER", List.of(new RequestMatcher(POST, "/book"), new RequestMatcher(GET, "/rental/books"), new RequestMatcher(GET, "/users")
+            "USER", List.of(new RequestMatcher(GET, "/books"), new RequestMatcher(POST, "/book/*/rental")),
+            "MANAGER", List.of(new RequestMatcher(POST, "/book"), new RequestMatcher(GET, "/users")
                     , new RequestMatcher(GET, "/books"), new RequestMatcher(GET, "/book/rental/users")),
             "NO_ROLE", List.of(new RequestMatcher(POST, "/user/login"), new RequestMatcher(POST, "/user"))
     );
 
     private final Map<AuthorizationMatcher, RequestMatcher> dynamicAuthorizationMatcher = Map.of(
-            new UserIdAuthorizationMatcher(), new RequestMatcher(GET, "/user/*/rental"),
-            new UserIdAuthorizationMatcher(), new RequestMatcher(DELETE, "/book/*/rental/user/*")
+            new UserIdAuthorizationMatcher(), new RequestMatcher(DELETE, "/book/*/rental/user/*"),
+            new UserIdAuthorizationMatcher(), new RequestMatcher(GET, "/book/rental/user/*")
     );
 
     public List<AuthorizationEntry> build() {
