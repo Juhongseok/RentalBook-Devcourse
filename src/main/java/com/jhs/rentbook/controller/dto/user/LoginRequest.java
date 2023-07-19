9842415de62f94782af6551770ea2c02e60b803e
JHS <wnghdtjr129@naver.com>
2023-07-19 13:55:05 +0900

    feat: 공백 문자도 검증에 포함되도록 설정

--- src/main/java/com/jhs/rentbook/controller/dto/user/LoginRequest.java
+++ src/main/java/com/jhs/rentbook/controller/dto/user/LoginRequest.java
@@ -1,11 +1,11 @@
 package com.jhs.rentbook.controller.dto.user;
 
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotBlank;
 
 public record LoginRequest(
-        @NotEmpty
+        @NotBlank
         String email,
-        @NotEmpty
+        @NotBlank
         String password
 ) {
 }
